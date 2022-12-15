3444c929cd84d65911d7a79aaa29e3edde0bc138
BDN-91 <buckatarudenis@gmail.com>
2022-12-15 16:23:50 +0200

    Start JS and modal winows

---
+++ JS/script.js
@@ -0,0 +1,86 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const overlay = document.querySelector('.overlay')
+	const buttonCall = document.querySelector('#call')
+	const buttonsClose = document.querySelectorAll('.close-modal')
+    const buttonCallBack = document.querySelector('#footer-call')
+    const allModals = document.querySelectorAll('.modal')
+
+	/*закриття модалки при кліку поза модалкою, на overlay*/
+	overlay.addEventListener('click', (event) => {
+		if (!event.target.classList.contains('modal') && !event.target.closest('.modal')) {
+			/*якщо об'єкт (event.target), куди клікнули,
+			* НЕ має класу 'modal' I і у клікнутого об'єкту
+			* НЕмає батьківського елементу із класом 'modal'*/
+			overlay.classList.remove('overlay--show')
+
+            for (i=0; i < allModals.length; i++) {
+                allModals[i].classList.remove('modal--show')
+            }
+
+		}
+	})
+
+	/*т.я. всі модалки є у верстці, отже і в DOM, створюємо
+	* listener для кожної із closeBtn в них*/
+	buttonsClose.forEach((button) => { /*почитати про оператори циклів*/
+		button.addEventListener('click', () => {
+			overlay.classList.remove('overlay--show')
+
+            for (i=0; i < allModals.length; i++) {
+                allModals[i].classList.remove('modal--show')
+            }
+		})
+	})
+
+	/*step modals list*/
+	const calcStepModal = document.querySelector('.modal.calculator')
+	const questionStepModal = document.querySelector('.modal.question')
+	const finalStepModal = document.querySelector('.modal.final')
+
+	/*open calc modal*/
+	buttonCall.addEventListener('click', () => {
+		overlay.classList.add('overlay--show')
+		calcStepModal.classList.add('modal--show')
+	})
+
+	/*first calc step*/
+	const calcBtn = calcStepModal.querySelector('.button')
+	/*querySelector може бути застосована не тільки до document,
+	* а й до раніше обраного блоку і пошук вузла за селектором
+	* буде виконаний в його межах*/
+	calcBtn.addEventListener('click', () => {
+		calcStepModal.classList.remove('modal--show') /*приховуємо поточний крок*/
+		questionStepModal.classList.add('modal--show') /*показуємо наступний крок*/
+	})
+
+	/*second calc step*/
+	const questionBtn = questionStepModal.querySelector('.button')
+	questionBtn.addEventListener('click', () => {
+		questionStepModal.classList.remove('modal--show')
+		finalStepModal.classList.add('modal--show')
+	})
+
+	/*third calc step*/
+	const finalBtn = finalStepModal.querySelector('.button')
+	finalBtn.addEventListener('click', () => {
+		finalStepModal.classList.remove('modal--show')
+		overlay.classList.remove('overlay--show')
+	})
+
+    /*callback modal*/
+
+    const callBackModal = document.querySelector('.callback.modal')
+
+    buttonCallBack.addEventListener('click', () => {
+		overlay.classList.add('overlay--show')
+		callBackModal.classList.add('modal--show')
+	})
+
+    const callBackBtn = callBackModal.querySelector('.button')
+
+    callBackBtn.addEventListener('click', () => {
+		callBackModal.classList.remove('modal--show')
+		overlay.classList.remove('overlay--show')
+	})
+
+})
