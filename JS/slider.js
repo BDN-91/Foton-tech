aa4782dfcbb2348671b3979927b1316bdc7d1ad8
BDN-91 <buckatarudenis@gmail.com>
2022-12-21 16:55:36 +0200

    Created JS for slider on Projects page

---
+++ JS/slider.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    const sliderBox = document.querySelector('.slider-box')
+    const slideLeft = document.querySelector('.button-left')
+    const slideRigft = document.querySelector('.button-right')
+    const itemsSlider = document.querySelectorAll('.slider-item')
+
+    sliderBox.addEventListener('click', (event)=> {
+
+        switch(event.target) {
+            case slideRigft:
+
+                for (let i = 0; i < itemsSlider.length; i++) {
+                     if (i == itemsSlider.length-1) {
+                        itemsSlider[itemsSlider.length-1].classList.remove('slider-item-show');
+                        itemsSlider[0].classList.add('slider-item-show');
+
+                    } else if (itemsSlider[i].classList.contains('slider-item-show') && itemsSlider[i].classList.contains('slider-item')) {
+                        itemsSlider[i].classList.remove('slider-item-show');
+                        itemsSlider[i+1].classList.add('slider-item-show');
+                        break;
+                    } 
+                }
+                break;
+            case slideLeft:
+
+                for (let i = itemsSlider.length-1; i >=0 ; i--) {
+                    if (i == 0) {
+                        itemsSlider[0].classList.remove('slider-item-show');
+                        itemsSlider[itemsSlider.length-1].classList.add('slider-item-show');
+
+                    } else if (itemsSlider[i].classList.contains('slider-item-show') && itemsSlider[i].classList.contains('slider-item')) {
+                        itemsSlider[i].classList.remove('slider-item-show');
+                        itemsSlider[i-1].classList.add('slider-item-show');
+                        break;
+                    } 
+                }
+                break; 
+        }
+    })
+
+    // itemsSlider[i].classList.remove('slider-item-show');
+    // itemsSlider[i].classList.add('slider-item-show');
+
+    // itemsSlider.forEach((item)=> {
+    //     item.classList.contains('.slider-item-show');
+    // console.log(item)
+
+    // })
+
+})
